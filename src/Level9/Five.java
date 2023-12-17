c8bae185898d57060db76913204d20a7114069c7
장근영 <jky00914@naver.com>
2023-12-17 22:34:28 +0900

    Baekjoon Level 9

--- src/Level9/Five.java
+++ src/Level9/Five.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Five {
-    public static boolean prime[];
+    public static boolean[] prime;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
--- src/Level9/Six.java
+++ src/Level9/Six.java
@@ -1,6 +1,5 @@
 package Level9;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
