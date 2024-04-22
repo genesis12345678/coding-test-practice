22194395af17b9ea581787ba3c00e94c76415630
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-04-22 16:19:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.63 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution{
+    
+    
+    public int solution(int []A, int []B){
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int sum = 0;
+        
+        for(int i = 0; i < A.length; i++){
+            sum += A[i] * B[B.length - 1 - i];
+        }
+        
+        return sum;
+    }
+    
+   
+}
