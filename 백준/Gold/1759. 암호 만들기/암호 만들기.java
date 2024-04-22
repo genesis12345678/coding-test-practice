1b4bcd7fdda5156858333d90805aef493c28785a
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-04-22 18:41:35 +0900

    [Gold V] Title: 암호 만들기, Time: 200 ms, Memory: 18000 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int L, C;
+    static ArrayList<String> list = new ArrayList<>();
+    static boolean[] visit = new boolean[16];
+    static char[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        arr = new char[C];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            arr[i] = st.nextToken().charAt(0);
+        }
+
+        Arrays.sort(arr);
+
+        backTrack(0, "");
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void backTrack(int index, String password) {
+        if (password.length() == L) {
+            if (isValidPassword(password)) {
+                list.add(password);
+            }
+            return;
+        }
+
+        for (int i = index; i < C; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                backTrack(i + 1, password + arr[i]);
+                visit[i] = false;
+            }
+        }
+    }
+
+    static boolean isValidPassword(String password) {
+
+        int vowel = 0;
+        int consonant = 0;
+
+        for (char ch : password.toCharArray()) {
+            if (isVowel(ch)) {
+                vowel++;
+            } else {
+                consonant++;
+            }
+        }
+
+        return vowel >= 1 && consonant >= 2;
+    }
+
+    private static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
