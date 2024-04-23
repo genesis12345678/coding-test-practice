0c57091af9e139ae350bd6c3d0bacab3c2bed9c8
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-04-23 10:05:00 +0900

    [Silver III] Title: N과 M (2), Time: 164 ms, Memory: 16272 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] visit = new boolean[9];
+    static int[] arr;
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        backTrack(0, "", 1);
+        System.out.print(sb);
+    }
+
+    private static void backTrack(int depth, String result, int start) {
+        if (depth == m) {
+            sb.append(result).append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                backTrack(depth + 1, result + i + " ", i + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
