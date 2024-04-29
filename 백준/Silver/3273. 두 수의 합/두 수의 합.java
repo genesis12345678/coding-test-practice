8e3e561652d63d3eabbd08f9b7a1fa5c78132a40
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-04-29 09:57:26 +0900

    [Silver III] Title: 두 수의 합, Time: 348 ms, Memory: 25900 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int x = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        int s = 0;
+        int e = n - 1;
+
+        int count = 0;
+
+        while (s < e) {
+            int sum = arr[s] + arr[e];
+            if (sum == x) {
+                count++;
+                s++;
+                e--;
+            } else if (sum > x) {
+                e--;
+            } else {
+                s++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
