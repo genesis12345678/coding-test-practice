4214f983cb64610fd6d062fc7ec8968bf323e8be
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-04-29 10:17:49 +0900

    [Silver III] Title: 두 수의 합, Time: 320 ms, Memory: 24812 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,15 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] sorted;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
+        sorted = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
@@ -18,7 +20,7 @@ public class Main {
 
         int x = Integer.parseInt(br.readLine());
 
-        Arrays.sort(arr);
+        mergeSort(arr, 0, n - 1);
 
         int s = 0;
         int e = n - 1;
@@ -38,7 +40,45 @@ public class Main {
             }
         }
 
-        System.out.println(count);
+        System.out.print(count);
     }
 
+    static void mergeSort(int[] arr, int start, int end) {
+        if (start == end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
+    }
+
+    static void merge(int[] arr, int start, int mid, int end) {
+        int left = start;
+        int right = mid + 1;
+        int index = start;
+
+        while (left <= mid && right <= end) {
+            if (arr[left] <= arr[right]) {
+                sorted[index++] = arr[left++];
+            } else {
+                sorted[index++] = arr[right++];
+            }
+        }
+
+        while (left <= mid) {
+            sorted[index++] = arr[left++];
+        }
+
+        while (right <= end) {
+            sorted[index++] = arr[right++];
+        }
+
+        for (int i = start; i <= end; i++) {
+            arr[i] = sorted[i];
+        }
+    }
 }
