05de30b890a5c5cee8fbbdd68aacbf5feabc5e63
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-05-05 10:16:01 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        Map<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        
+        int result = 0;
+        Map<String, Integer> disMap = new HashMap<>();
+        for (int i = 0; i <= discount.length - 10; i++) {
+            boolean find = true;
+
+            for (int j = i; j < i + 10; j++) {
+                disMap.put(discount[j], disMap.getOrDefault(discount[j], 0) + 1);
+            }
+
+            for (String s : want) {
+                if (!Objects.equals(wantMap.get(s), disMap.get(s))) {
+                    find = false;
+                    break;
+                }
+            }
+            if (find) {
+                result++;
+            }
+
+            disMap.clear();
+        }
+
+        return result;
+    }
+}
