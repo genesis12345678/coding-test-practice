f680875f3c92f8e634266eb1b5ef07078265b8ea
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-05-11 09:48:50 +0900

    [Silver II] Title: N번째 큰 수, Time: 1948 ms, Memory: 338092 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        System.out.println(list.get(n - 1));
+    }
+}
