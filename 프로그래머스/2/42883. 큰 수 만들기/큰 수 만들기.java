5a6370a5ea3ff753bfe5b500957f07743ef56a51
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-05 21:41:52 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.29 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            char ch = number.charAt(i);
+
+            while (!stack.isEmpty() && ch > stack.peek() && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(ch);
+        }
+
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
